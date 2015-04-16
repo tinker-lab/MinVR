97fe388b8823373bedf42de26667234e1a045bfc
Dan Orban <dtorban@umn.edu>
2015-04-16 07:28:03 -0500

    Added GL header.

--- Plugins/Devices/OculusRift/OculusRiftDisplay.cpp
+++ Plugins/Devices/OculusRift/OculusRiftDisplay.cpp
@@ -8,27 +8,97 @@
 
 #include "OculusRiftDisplay.h"
 
-#if defined(WIN32)
-#define NOMINMAX
-#include <windows.h>
-#include <GL/gl.h>
-#elif defined(__APPLE__)
-#include <OpenGL/OpenGL.h>
-#include <OpenGL/glu.h>
-#else
-#define GL_GLEXT_PROTOTYPES
-#include <GL/gl.h>
-#include <GL/glu.h>
-#endif
-#include "GL/glext.h"
-
-#include <string.h>
-
-using namespace glm;
 
 namespace MinVR {
 
+
+
+namespace ovr {
+
+  // Convenience method for looping over each eye with a lambda
+  template <typename Function>
+  inline void for_each_eye(Function function) {
+    for (ovrEyeType eye = ovrEyeType::ovrEye_Left;
+      eye < ovrEyeType::ovrEye_Count;
+      eye = static_cast<ovrEyeType>(eye + 1)) {
+      function(eye);
+    }
+  }
+
+  inline mat4 toGlm(const ovrMatrix4f & om) {
+    return glm::transpose(glm::make_mat4(&om.M[0][0]));
+  }
+
+  inline mat4 toGlm(const ovrFovPort & fovport, float nearPlane = 0.01f, float farPlane = 10000.0f) {
+    return toGlm(ovrMatrix4f_Projection(fovport, nearPlane, farPlane, true));
+  }
+
+  inline vec3 toGlm(const ovrVector3f & ov) {
+    return glm::make_vec3(&ov.x);
+  }
+
+  inline vec2 toGlm(const ovrVector2f & ov) {
+    return glm::make_vec2(&ov.x);
+  }
+
+  inline uvec2 toGlm(const ovrSizei & ov) {
+    return uvec2(ov.w, ov.h);
+  }
+
+  inline quat toGlm(const ovrQuatf & oq) {
+    return glm::make_quat(&oq.x);
+  }
+
+  inline mat4 toGlm(const ovrPosef & op) {
+    mat4 orientation = glm::mat4_cast(toGlm(op.Orientation));
+    mat4 translation = glm::translate(mat4(), ovr::toGlm(op.Position));
+    return translation * orientation;
+  }
+
+  inline ovrMatrix4f fromGlm(const mat4 & m) {
+    ovrMatrix4f result;
+    mat4 transposed(glm::transpose(m));
+    memcpy(result.M, &(transposed[0][0]), sizeof(float) * 16);
+    return result;
+  }
+
+  inline ovrVector3f fromGlm(const vec3 & v) {
+    ovrVector3f result;
+    result.x = v.x;
+    result.y = v.y;
+    result.z = v.z;
+    return result;
+  }
+
+  inline ovrVector2f fromGlm(const vec2 & v) {
+    ovrVector2f result;
+    result.x = v.x;
+    result.y = v.y;
+    return result;
+  }
+
+  inline ovrSizei fromGlm(const uvec2 & v) {
+    ovrSizei result;
+    result.w = v.x;
+    result.h = v.y;
+    return result;
+  }
+
+  inline ovrQuatf fromGlm(const quat & q) {
+    ovrQuatf result;
+    result.x = q.x;
+    result.y = q.y;
+    result.z = q.z;
+    result.w = q.w;
+    return result;
+  }
+}
+
 OculusRiftDisplay::OculusRiftDisplay() {
+	if (!ovr_Initialize()) {
+	    FAIL("Failed to initialize the Oculus SDK");
+	}
+
 	_hmd = ovrHmd_Create(0);
 	if (nullptr == _hmd) {
 		ovrHmdType defaultHmdType = ovrHmd_DK2;
@@ -39,11 +109,28 @@ OculusRiftDisplay::OculusRiftDisplay() {
 		_hmdDesktopPosition = ivec2(_hmd->WindowsPos.x, _hmd->WindowsPos.y);
 		_hmdNativeResolution = ivec2(_hmd->Resolution.w, _hmd->Resolution.h);
 	}
+
+    /*if (!ovrHmd_ConfigureTracking(_hmd,
+      ovrTrackingCap_Orientation | ovrTrackingCap_Position | ovrTrackingCap_MagYawCorrection, 0)) {
+      FAIL("Could not attach to sensor device");
+    }*/
+
+    memset(_eyeTextures, 0, 2 * sizeof(ovrGLTexture));
+
+    ovr::for_each_eye([&](ovrEyeType eye){
+      ovrSizei eyeTextureSize = ovrHmd_GetFovTextureSize(_hmd, eye, _hmd->MaxEyeFov[eye], 1.0f);
+      ovrTextureHeader & eyeTextureHeader = _eyeTextures[eye].Header;
+      eyeTextureHeader.TextureSize = eyeTextureSize;
+      eyeTextureHeader.RenderViewport.Size = eyeTextureSize;
+      eyeTextureHeader.API = ovrRenderAPI_OpenGL;
+    });
 }
 
 OculusRiftDisplay::~OculusRiftDisplay() {
 	ovrHmd_Destroy(_hmd);
 	_hmd = nullptr;
+
+	ovr_Shutdown();
 }
 
 void OculusRiftDisplay::render(int threadId, WindowRef window, AbstractMVRAppRef app) {
--- Plugins/Devices/OculusRift/OculusRiftDisplay.h
+++ Plugins/Devices/OculusRift/OculusRiftDisplay.h
@@ -11,7 +11,98 @@
 
 #include "MVRCore/Display/StereoDisplayFactory.h"
 
+#include <iostream>
+#include <memory>
+#include <exception>
+
+
+#if defined(OS_WIN)
+#define ON_WINDOWS(runnable) runnable()
+#define ON_MAC(runnable)
+#define ON_LINUX(runnable)
+#define NOMINMAX
+#define WIN32_LEAN_AND_MEAN
+#include <Windows.h>
+#undef NOMINMAX
+#elif defined(OS_OSX)
+#define ON_WINDOWS(runnable)
+#define ON_MAC(runnable) runnable()
+#define ON_LINUX(runnable)
+#elif defined(OS_LINUX)
+#define ON_WINDOWS(runnable)
+#define ON_MAC(runnable)
+#define ON_LINUX(runnable) runnable()
+#endif
+
+
+#define __STDC_FORMAT_MACROS 1
+#define GLM_FORCE_RADIANS
+
+#define FAIL(X) throw std::runtime_error(X)
+
+#ifdef OVR_OS_LINUX
+#pragma GCC diagnostic ignored "-Wshadow"
+#pragma GCC diagnostic ignored "-Wunused-parameter"
+#pragma GCC diagnostic ignored "-Wunused-variable"
+#pragma GCC diagnostic ignored "-Wpedantic"
+#pragma GCC diagnostic ignored "-Wunknown-pragmas"
+#endif
+
+///////////////////////////////////////////////////////////////////////////////
+//
+// GLM is a C++ math library meant to mirror the syntax of GLSL
+//
+
+#define GLM_FORCE_RADIANS
 #include <glm/glm.hpp>
+#include <glm/gtc/noise.hpp>
+#include <glm/gtx/rotate_vector.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtx/transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#include <glm/gtc/quaternion.hpp>
+#include <glm/gtx/quaternion.hpp>
+
+// Import the most commonly used types into the default namespace
+using glm::ivec3;
+using glm::ivec2;
+using glm::uvec2;
+using glm::mat3;
+using glm::mat4;
+using glm::vec2;
+using glm::vec3;
+using glm::vec4;
+using glm::quat;
+
+
+//////////////////////////////////////////////////////////////////////
+//
+// The OVR types header contains the OS detection macros:
+//  OVR_OS_WIN32, OVR_OS_MAC, OVR_OS_LINUX (and others)
+//
+
+#if (defined(WIN64) || defined(WIN32))
+#define OS_WIN
+#elif (defined(__APPLE__))
+#define OS_OSX
+#else
+#define OS_LINUX
+#endif
+
+#if defined(WIN32)
+#define NOMINMAX
+#include <windows.h>
+#include <GL/gl.h>
+#elif defined(__APPLE__)
+#include <OpenGL/OpenGL.h>
+#include <OpenGL/glu.h>
+#else
+#define GL_GLEXT_PROTOTYPES
+#include <GL/gl.h>
+#include <GL/glu.h>
+#endif
+#include "GL/glext.h"
+
 // The oculus SDK requires it's own macros to distinguish between
 // operating systems, which is required to setup the platform specific
 // values, like window handles
@@ -27,6 +118,117 @@
 
 namespace MinVR {
 
+struct FramebufferWraper {
+  glm::uvec2 size;
+  GLuint fbo{0};
+  GLuint color{0};
+  GLuint depth{ 0 };
+
+  virtual ~FramebufferWraper() {
+    release();
+  }
+
+  void allocate() {
+    release();
+    glGenRenderbuffers(1, &depth);
+    assert(depth);
+    glGenTextures(1, &color);
+    assert(color);
+    glGenFramebuffers(1, &fbo);
+    assert(fbo);
+  }
+
+  void release() {
+    if (fbo) {
+      glDeleteFramebuffers(1, &fbo);
+      fbo = 0;
+    }
+    if (color) {
+      glDeleteTextures(1, &color);
+      color = 0;
+    }
+    if (depth) {
+      glDeleteRenderbuffers(1, &depth);
+      depth = 0;
+    }
+  }
+
+  static bool checkStatus(GLenum target = GL_FRAMEBUFFER) {
+    GLuint status = glCheckFramebufferStatus(target);
+    switch (status) {
+    case GL_FRAMEBUFFER_COMPLETE:
+      return true;
+      break;
+
+    case GL_FRAMEBUFFER_INCOMPLETE_ATTACHMENT:
+      std::cerr << "framebuffer incomplete attachment" << std::endl;
+      break;
+
+    case GL_FRAMEBUFFER_INCOMPLETE_MISSING_ATTACHMENT:
+      std::cerr << "framebuffer missing attachment" << std::endl;
+      break;
+
+    case GL_FRAMEBUFFER_INCOMPLETE_DRAW_BUFFER:
+      std::cerr << "framebuffer incomplete draw buffer" << std::endl;
+      break;
+
+    case GL_FRAMEBUFFER_INCOMPLETE_READ_BUFFER:
+      std::cerr << "framebuffer incomplete read buffer" << std::endl;
+      break;
+
+    case GL_FRAMEBUFFER_INCOMPLETE_MULTISAMPLE:
+      std::cerr << "framebuffer incomplete multisample" << std::endl;
+      break;
+
+    case GL_FRAMEBUFFER_INCOMPLETE_LAYER_TARGETS:
+      std::cerr << "framebuffer incomplete layer targets" << std::endl;
+      break;
+
+    case GL_FRAMEBUFFER_UNSUPPORTED:
+      std::cerr << "framebuffer unsupported internal format or image" << std::endl;
+      break;
+
+    default:
+      std::cerr << "other framebuffer error" << std::endl;
+      break;
+    }
+
+    return false;
+  }
+
+  void init(const glm::ivec2 & size) {
+    this->size = size;
+    allocate();
+
+    glBindFramebuffer(GL_FRAMEBUFFER, fbo);
+
+    glBindTexture(GL_TEXTURE_2D, color);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_BORDER);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_BORDER);
+    glTexStorage2D(GL_TEXTURE_2D, 1, GL_RGBA8, size.x, size.y);
+    glBindTexture(GL_TEXTURE_2D, 0);
+    glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, color, 0);
+
+    glBindRenderbuffer(GL_RENDERBUFFER, depth);
+    glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH_COMPONENT16, size.x, size.y);
+    glBindRenderbuffer(GL_RENDERBUFFER, 0);
+    glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_RENDERBUFFER, depth);
+
+    if (!checkStatus()) {
+      FAIL("Could not create a valid framebuffer");
+    }
+    glBindFramebuffer(GL_FRAMEBUFFER, 0);
+  }
+
+  void activate() {
+    assert(fbo);
+    glBindFramebuffer(GL_FRAMEBUFFER, fbo);
+    glViewport(0, 0, size.x, size.y);
+  }
+};
+
 class OculusRiftDisplay : public StereoDisplay {
 public:
 	OculusRiftDisplay();
@@ -38,7 +240,13 @@ private:
 	ovrHmd _hmd;
 	glm::uvec2 _hmdNativeResolution;
 	glm::ivec2 _hmdDesktopPosition;
+	ovrTexture _eyeTextures[2];
+	ovrVector3f _eyeOffsets[2];
+	ovrEyeType _currentEye{ ovrEye_Count };
 	ovrEyeRenderDesc _eyeRenderDescs[2];
+	glm::mat4 _projections[2];
+	ovrPosef _eyePoses[2];
+	FramebufferWraper _eyeFbos[2];
 };
 
 class OculusRiftDisplayFactory : public StereoDisplayFactory {
